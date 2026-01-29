2c2a467e5dc37fb30cc130ce9deb314dddc4f71d
Dmitry Sazonov <dsazonoff@gmail.com>
2026-01-29 20:03:27 +0100

    Windows build fix

--- build.bat
+++ build.bat
@@ -7,13 +7,13 @@ set BUILD_DIR_X64=build\release_windows_x64
 
 cmake -S . -B "%BUILD_DIR_X64%" ^
   -DCMAKE_BUILD_TYPE=Release ^
-  -DCMAKE_PREFIX_PATH=third-party\x64-windows-static ^
+  -DCMAKE_PREFIX_PATH=third-party\ARM64 ^
   -G "Visual Studio 17 2022" ^
   -A x64
 
 cmake --build "%BUILD_DIR_X64%" --config Release
 if not exist "%INSTALL_DIR%" mkdir "%INSTALL_DIR%"
-copy "%BUILD_DIR_X64%\bin\%BIN_NAME%" "%INSTALL_DIR%\" >nul
+copy "%BUILD_DIR_X64%\Release\%BIN_NAME%" "%INSTALL_DIR%\" >nul
 if errorlevel 1 exit /b 1
 
 echo Build finished successfully.
